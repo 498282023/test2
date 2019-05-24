ed50b73bb68847dfc44804e6cc6f283aefcc32ad
wusili <498282023@qq.com>
2019-05-25 00:18:19 +0800

    增加点击图片时徘徊4s功能

--- text2/JavaScript.js
+++ text2/JavaScript.js
@@ -4,6 +4,8 @@ var goPreBtn = document.getElementById("goPre")
 var goNextBtn = document.getElementById("goNext");
 var index = 0;//index表示第几张图片，第几张图片就会有active这个类名
 var points = document.getElementsByClassName("point");//点
+var time = 0; var stop = 4;//定时器,图片徘徊参数
+
 var clearActive = function () {
     for (var i = 0; i < items.length; i++) {
         items[i].className = "item";
@@ -14,6 +16,7 @@ var goIndex = function () {
     clearActive();
     items[index].className = "item active";
     points[index].className = "point active";
+
 }
 var goNext = function () {
     if (index < 4) {
@@ -23,7 +26,8 @@ var goNext = function () {
         index = 0;
     }
     goIndex();
-}
+    time =time-stop;//图片徘徊参数
+}    
 var goPre = function () {
     if (index > 0) {
         index--;
@@ -31,6 +35,7 @@ var goPre = function () {
         index = 4;
     }
     goIndex();
+    time =time- stop;//图片徘徊参数
 }
 //为什么外部引用添加不了句柄
 goNextBtn.addEventListener("click", goNext);
@@ -40,5 +45,16 @@ for (var i = 0; i < points.length; i++) {
         index = this.getAttribute("data-index");
         console.log(index)
         goIndex();
+        time = time - stop;//图片徘徊参数
     })
 }
+
+//轮播T=2s,鼠标翻页时徘徊4s;
+setInterval(function () {
+    time++;
+    if (time == 2) {
+        goNext();
+        time = 0;
+    }
+
+},1000)
